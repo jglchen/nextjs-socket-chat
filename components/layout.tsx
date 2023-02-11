da3e0e87699f5d952d5d903ca2862eddbd69f378
John Chen <jglchen@gmail.com>
2023-02-11 21:55:27 +0800

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,6 +6,15 @@ export default function Layout({ children}: {children: JSX.Element}) {
            <Head>
              <title>Chat Applications with Socket.IO</title>
              <link rel="icon" href="/favicon.ico" />
+             <meta
+              name="description"
+              content="A real-time chat application with Socket.IO"
+              />
+             <meta name="og:title" content="Chat Applications with Socket.IO" />
+             <meta
+              property="og:description"
+              content="A real-time chat application with Socket.IO"
+              />
            </Head>
            <main>
               {children}
